896d76faa4fbc6256e5ff31998050eac2dd4a214
Hemwatie Persaud <hemwatie.persaud@gmail.com>
2021-11-01 23:05:34 -0400

    used useEffect and useState to address async isssues

--- src/UI/Data.jsx
+++ src/UI/Data.jsx
@@ -1,40 +1,37 @@
-import React, { Component }  from 'react';
+import React from 'react';
+import Table from './Table';
 
 function Data() {
-    const [data, setData] = React.useState([]);
-    React.useEffect(fetchData, [])
-
-    function fetchData(){
-          fetch('http://localhost:3000/allData', {
-              method: "GET",
-              headers: {
-                  'Content-Type': 'application/json'
-              }
+    const [cryptoData, setData] = React.useState([]);
+    
+    React.useEffect(()=>{
+        fetchData();
+        function fetchData(){
+        fetch('http://localhost:3000/hello',{
+            method: "GET",
+            mode: "cors",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+          .then((response)=>response.json())
+          .then((response)=> {
+              const holdData = [];
+              const responseData = response.data;
+              responseData.forEach(ele => {
+                holdData.push(ele)
+              })
+              setData(holdData);
           })
-            .then((response)=>{
-                response.json()
-                console.log('response first then: ', response)
-            })
-            .then((response)=>{
-                response.forEach((ele) => {
-                    data.push(ele);
-                })
-                setData(data);
-            })
-            .catch((error) =>{
-                console.log('Frontend error in /allData GET request: ', error);
-            })
-    }
+          .catch((error) =>{
+              console.log('Frontend error in /hello GET request: ', error);
+          })
+        }}, []);
 
-    const display = [];
-    for(let i = 0; i < 5; i++){
-        console.log('data[i]', data[i])
-        display.push(<div><h1>{data[i]}</h1></div>)
-    }
 
     return (
-        <div>asdf
-            {display}
+        <div>
+            <Table cryptoData = {cryptoData}/>
         </div>
     )
     
